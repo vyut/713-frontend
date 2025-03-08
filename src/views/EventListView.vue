f9a71d0fa27dbff8e6c709d688b618f32cd980f5
vyut <vyut@msn.com>
2025-03-08 19:14:01 +0700

    Task 3 - v-for

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import EventCard from '@/components/EventCard.vue'
+
+const events = ref([
+  { id: 1, name: 'Event 1' },
+  { id: 2, name: 'Event 2' },
+  { id: 3, name: 'Event 3' },
+])
 </script>
 
 <template>
   <div>
-    <EventCard />
-    <EventCard />
-    <EventCard />
+    <EventCard v-for="event in events" :key="event.id" />
   </div>
 </template>
