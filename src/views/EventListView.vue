279a5ed530bc7ba6a3948c18a7138a39f156e18e
vyut <vyut@msn.com>
2025-03-08 20:07:24 +0700

    Task 6 - การเรียกใช้ backend application

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,48 +1,18 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 import EventCard from '@/components/EventCard.vue'
+import EventService from '@/services/EventService'
+import { ref } from 'vue'
+import type { Event } from '@/types'
 
-const events = ref([
-  {
-    id: 1,
-    category: 'Category 1',
-    title: 'Event 1 - Vue.js',
-    description: 'Vue.js Meetup',
-    date: '2021-10-10',
-    time: '10:00',
-    location: 'Location 1',
-    petsAllowed: true,
-    organizer: {
-      name: 'Organizer 1',
-    },
-  },
-  {
-    id: 2,
-    category: 'Category 2',
-    title: 'Event 2 - React',
-    description: 'React Meetup',
-    date: '2021-10-11',
-    time: '11:00',
-    location: 'Location 2',
-    petsAllowed: false,
-    organizer: {
-      name: 'Organizer 2',
-    },
-  },
-  {
-    id: 3,
-    category: 'Category 3',
-    title: 'Event 3 - Angular',
-    description: 'Angular Meetup',
-    date: '2021-10-12',
-    time: '12:00',
-    location: 'Location 3',
-    petsAllowed: true,
-    organizer: {
-      name: 'Organizer 3',
-    },
-  },
-])
+const events = ref<Event[]>([])
+
+interface EventResponse {
+  data: Event[]
+}
+
+EventService.getEvents().then((response: EventResponse) => {
+  events.value = response.data
+})
 </script>
 
 <template>
