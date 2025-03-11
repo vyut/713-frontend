900fee5b238539dc6ae069fa08e3a4723c2367c0
vyut <vyut@msn.com>
2025-03-11 12:01:00 +0700

    Task 12 - การจัดการ Error handler ใน Vue - Task 1

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import EventLayoutView from '@/views/event/LayoutView.vue'
 import EventDetailView from '@/views/event/DetailView.vue'
 import EventRegisterView from '@/views/event/RegisterView.vue'
 import EventEditView from '@/views/event/EditView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -46,6 +47,11 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: () => import('../views/AboutView.vue'),
+    },
+    {
+      path: '/:catchAll(.*)',
+      name: 'not-found',
+      component: NotFoundView,
     },
   ],
 })
